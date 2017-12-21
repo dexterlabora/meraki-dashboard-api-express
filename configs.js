d0bd82d3f0a4e19c86e17df549125f2016c293ae
Cory Guynn <cory@premiereportal.net>
2017-12-21 11:02:03 +0100

    first commit

---
+++ configs.js
@@ -0,0 +1,7 @@
+// Define your environment variables here
+
+module.exports = 
+{
+    apiKey: 'be647eed3046542a895ea9ad07b7f7cef4c002de',
+    apiUrl: 'https://api.meraki.com/api/v0'
+}
---
+++ server.js
@@ -0,0 +1,114 @@
+/* ***************** /
+Meraki Dashoard API Express Server
+
+Supports redirects with custom request-meraki function.
+
+/ ****************** */
+
+
+// Environment  Variables
+
+// External Configuration File
+var configs = require('./configs.js');
+
+/* Local Configuration alternative
+var configs = {
+    apiKey: 'YourAPIKey',
+    apiUrl: 'https://api.meraki.com/api/v0'
+};
+*/
+
+
+/* ****************** */
+
+var express = require('express');
+//var request = require('request'); // Does not properly handle Meraki redirects
+var requestMeraki = require('./request-meraki');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = module.exports = express();
+
+// Development Tools
+/*
+var morgan = require('morgan');
+app.use(morgan('dev'))
+
+var globalLog = require('global-request-logger');
+globalLog.initialize();
+
+globalLog.on('success', function(request, response) {
+  console.log('SUCCESS');
+  console.log('Request', request);
+  console.log('Response', response);
+});
+ 
+globalLog.on('error', function(request, response) {
+  console.log('ERROR');
+  console.log('Request', request);
+  console.log('Response', response);
+});
+*/
+
+
+var jsonParser = bodyParser.json();
+
+// API Route - Will be proxied through Meraki Dashboard API
+app.use('/api', jsonParser, function (req, res){
+  console.log('API request url', req.url);
+  console.log('API request headers', req.headers);
+  console.log('request body, ', req.body);
+  
+  // Use client supplied API key or default to server config.
+  var apiKey = '';
+  if('x-cisco-meraki-api-key' in req.headers){
+    apiKey = req.headers['x-cisco-meraki-api-key'];
+    console.log("New headers sent", apiKey );
+  }else{
+    apiKey = configs.apiKey; 
+  }
+ 
+  var options = {
+    qs: req.query,
+    url: configs.apiUrl + req.url,
+    method: req.method,
+    body: JSON.stringify(req.body), 
+    //followAllRedirects: true, // Does not work as intended with PUT,POST,DELETE (returns a [GET] on final location)
+    headers: {
+        'X-Cisco-Meraki-API-Key': apiKey,
+        'Content-Type': 'application/json'
+    } 
+  }
+
+  requestMeraki(options, function(err, response, data){
+    if(err){
+        console.log("requestMeraki err ", err)
+        res.status(response.statusCode).send({
+            message: 'err'
+         });
+        res.send(err);
+    }
+    console.log('FINAL res.statusCode ',response.statusCode);
+    console.log('FINAL res.body ',response.body);
+
+    res.setHeader('content-type', response.headers['content-type']);
+    res.status(response.statusCode).send(data);
+    
+    
+  });
+
+});
+
+
+
+// Home page, default route
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+// Start server
+var port = process.env.PORT || 8085;
+var server = app.listen(port, () => {
+  console.log('Server Running on '+ server.address().address+port+'/');
+  console.log('Meraki API Proxy: localhost:'+server.address().address+ ':' +port+'/api');
+  console.log('Default API Endpoint: ', configs.apiUrl);
+});
+
